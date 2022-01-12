40d6ce091b1c796d33846721d5069d72cd6f115f
Areeba Khan <kahnareeba@gmail.com>
2022-01-12 14:13:46 -0600

    Scrapes IMDB rating

--- src/commands/watch.js
+++ src/commands/watch.js
@@ -26,31 +26,68 @@ module.exports = {
             //     const searchTitle = $(this).text();
             //     queryReply['searchTitle'] = searchTitle;
             // });
+            const searchTitle = $('.title-block').text().replace(/\s/,'');
+            queryReply['searchTitle'] = searchTitle; 
 
-            const searchTitle = $('.title-block', html).text();
-            queryReply['searchTitle'] = searchTitle;
+            const streamIcons = $('.monetizations', html)
+                    .children().next().attr('class'); // price-comparison__grid__row price-comparison__grid__row--stream
+            //         .children().next().attr('class'); // price-comparison__grid__row__holder
+                    
+            // $('.' + streamIcons, html).each(function () {
+            //     console.log(">>>>" + $(this).children().attr('class'));
+            // })
 
-            $('.price-comparison__grid__row__icon', html).each(function() {
-                const serviceTitle = $(this).attr('title');
-                console.log(serviceTitle)  
-            })
+            // const streamElements = $('.monetizations', html).children().next()
+            //     .children().next() // //price comparison grid row holder
+            //     .children().attr('class');
 
+            const imdbRating = $('.jw-scoring-listing').children().next().find('a').text().split(' ')[1];
+            queryReply['imdbRating'] = imdbRating;
+            
+            // $('.jw-scoring-listing__rating').each(function ()  {
+            //     const curIcon = $(this).children().next() // price-comparison__grid__row__icon
+            //     // .children().next() // //price comparison grid row holder
+            //     // .children();
+
+
+            //     console.log('>>>>' + $(this).text());
+            // });
+
+                    // .children().find('.price-comparison__grid__row__holder').attr('class');
+                
+                    
+                    // .each(function () {
+                    //     const curIcon = $(this).text();
+                    //     console.log(curIcon);
+                    // });
+
+
+            // console.log(streamIcons);
+
+            // $('.price-comparison__grid__row__icon', html).each(function() {
+            //     const serviceTitle = $(this).attr('title');
+            //     console.log(serviceTitle)  
+            // })
+
+            //price-comparison__grid__row price-comparison__grid__row--stream
             queryReply['genre'] = genre;
 
             console.log(queryReply);
+
+            message.reply(`**${queryReply.searchTitle}**\n> ${queryReply.genre}\n> IMDB: **${queryReply.imdbRating}**.`);
             return queryReply;
         }
-
+        
         axios( url + searchCategory[0] + args.join('-') )
             .then(response => {
-                let queryReply = scrapeResponse(response, searchCategory[0].split('/')[0].split('-').join(' ').toUpperCase()); 
+                let queryReply = scrapeResponse(response, searchCategory[0].split('/')[0].split('-').join(' ')); 
 
             }).catch(
                 err => { // check if its a tv show: 
                     axios( url + searchCategory[1] + args.join('-') )
                         .then(response => {
                             let queryReply =  scrapeResponse(response, 
-                                searchCategory[1].split('/')[0].split('-').join(' ').toUpperCase());
+                                searchCategory[1].split('/')[0].split('-').join(' '));
                         }).catch ( err => {
                             console.log(err);
                             message.reply(`There was an error in looking for "${args.join(' ')}."`)
