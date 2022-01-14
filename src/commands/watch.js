d9eca221390bdbb62c328281d22cb02fc12cabd9
Areeba Khan <kahnareeba@gmail.com>
2022-01-13 20:09:06 -0600

    Fixes issues in pulling names of services and repitition of services

--- src/commands/watch.js
+++ src/commands/watch.js
@@ -1,7 +1,8 @@
-const { html } = require('cheerio/lib/api/manipulation');
 const fs = require('fs');
 const cheerio = require('cheerio');
 const axios = require('axios');
+const { html } = require('cheerio/lib/api/manipulation');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: "watch",
@@ -18,24 +19,78 @@ module.exports = {
 
         function scrapeTitle(htmlBlock) {  return htmlBlock.text().replace(/\s/,'').split(')')[0] + ')'; }
 
-        function scrapeStreams(htmlBlock) { 
+        function scrapeStreams($) { 
             let listStream = [];
-            htmlBlock = htmlBlock.children().next().children().next().children();
+            let servicesPushed = [];
 
-            while(htmlBlock.find('img').attr('title')) {
-                let tempService = { 'name':  htmlBlock.find('img').attr('title')};
-                listStream.push({ 
-                    'name': htmlBlock.find('img').attr('title'),    // scrapes service name
-                    'link': htmlBlock.find('a').attr('href')        // scrapes service url
-                });
-                htmlBlock = htmlBlock.next();
-            }
+            // htmlBlock = htmlBlock.children().next().children().next().children();
+            // htmlBlock = htmlBlock.children().next().children().children() //price-comparison__grid__row__element
+            //price-comparison__grid__row__element
+            $('.price-comparison__grid__row.price-comparison__grid__row--stream a').each(function () {
+                
+                let tempStreamObj = { 
+                    'name': $(this).find('img').attr('title'),    // scrapes service name
+                    'link': $(this).attr('href'),       // scrapes service url
+                    'subtext': $(this).find('div').text()
+                }
+                    // listStream.push();
+                if(servicesPushed.indexOf(tempStreamObj.name) === -1) {
+                    listStream.push(tempStreamObj); 
+                    servicesPushed.push(tempStreamObj.name);
+                    }
+                // console.log(`${$(this).html()}`)
+                // console.log('-------')
+            });
+            // console.log(`${htmlBlock.html()}`)
+
+            // while(htmlBlock.find('img').attr('title')) {
+            //     let tempService = { 'name':  htmlBlock.find('img').attr('title')};
+            //     listStream.push({ 
+            //         'name': htmlBlock.find('img').attr('title'),    // scrapes service name
+            //         // 'link': htmlBlock.find('a').attr('href'),       // scrapes service url
+            //         // 'subtext': htmlBlock.find('a').children().next().text()
+            //     });
+            //     htmlBlock = htmlBlock.next();
+            // }
 
             return listStream;
         }
 
         function scrapeRating(htmlBlock) { return htmlBlock.children().next().find('a').text().split(' ')[1]; }
         function scrapePoster(htmlBlock) { return htmlBlock.find('picture').find('source').attr('data-srcset').split(',')[0]; }
+
+        function createServiceFields(reply) {
+            // takes a list of JSON objects and returns a list of fields containing json objects
+            services = reply.listStream;
+            
+            let returnFields = [
+                {
+                    name: 'SYNOPSIS',
+                },
+                {
+                    name: '\u200b',
+                    value: '\u200b',
+                    inline: false,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+            ];
+              
+            return returnFields;
+        }
 
         function embedSend(replyObject) {
             if (message.author.id === client.user.id) return;
@@ -46,14 +101,14 @@ module.exports = {
                     icon_url: `${client.user.displayAvatarURL()}`
                 },
                 title: `${replyObject.searchTitle}`,
-                description: `${replyObject.genre.toUpperCase()}`,
+                description: `${replyObject.genre.toUpperCase()}   :arrow_right:   IMDB rating: **${replyObject.imdbRating}**`,
                 color: '#d32256',
                 image: {
                     url: `${replyObject.posterURL}`,
                 },
             }
 
-            const serviceEmbed = {
+            let serviceEmbed = {
                 description: `Where to watch **${replyObject.searchTitle}**?`,
                 color: '#d32256',
                 timestamp: new Date(),
@@ -62,6 +117,11 @@ module.exports = {
                     icon_url: 'https://upload.wikimedia.org/wikipedia/commons/e/e1/JustWatch.png',
                 },
             }
+
+            serviceEmbed = new MessageEmbed(serviceEmbed);
+            serviceEmbed.addFields(createServiceFields(replyObject));
+            
+            
 
             message.channel.send({embeds: [bannerEmbed, serviceEmbed]});
         }
@@ -75,7 +135,9 @@ module.exports = {
             queryReply['posterURL'] = scrapePoster( $('.jw-info-box__container-sidebar') );
             queryReply['genre'] = genre;
             queryReply['imdbRating'] = scrapeRating( $('.jw-scoring-listing') );
-            queryReply['listStream'] = scrapeStreams( $('.monetizations') );
+            // queryReply['listStream'] = scrapeStreams( $('.monetizations') );
+            queryReply['listStream'] = scrapeStreams( $ );
+
 
             console.log(queryReply);
 
