6a9eeed1a7691d26cf3f75459af630f94b8ee16f
Areeba Khan <kahnareeba@gmail.com>
2022-01-15 00:43:49 -0600

    Finishes first draft of video to gif creater

--- src/commands/videoToGyf.js
+++ src/commands/videoToGyf.js
@@ -2,66 +2,61 @@ require('dotenv').config();
 const fs = require('fs');
 const axios = require('axios');
 
-module.exports = {
-    name: 'v2g',
-    description: "Sends a video to gyfcat api and recieves a gif of it",
-    async execute(client, message, args, Discord) {
-        let token_params = {
-            client_id: process.env.GYFCAT_ID,
-            client_secret: process.env.GYFCAT_SECRET,
-            grant_type: 'client_credentials'
-        };
-
-        let createGIF = {
-            fetchUrl: 'https://cdn.discordapp.com/attachments/730624878692008009/931087576838860870/20220112_203712.mp4',
-            title: 'usergif'
-        };
-
-        let gifOBJ = {
-            gfyname: 'SizzlingFickleKiskadee',
-            secret: 'smjn9o9o6q',
-
-            // gfyname2: 'UnrulyDimpledJavalina',
-            // secret2: '66iscrmz1y6',
-
-            access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NDIyMTcyMjcsImlzcyI6IjJfeHFWbDRBIiwicm9sZXMiOlsiQ29udGVudF9SZWFkZXIiXX0.cqWEbzMR7HWJ6fMeO6_tyLfVFTM36pFXMKbY5OmlwV4'
-        }
-
-        // get request to check if task upload was complete for the given gyfname. 
-        const response = await axios.get(`https://api.gfycat.com/v1/gfycats/fetch/status/${gifOBJ.gfyname}`)
-        console.log(response.data);
-
-        // post request to create a gif, returns name of give, secret code...
-        // const response = await axios.post('https://api.gfycat.com/v1/gfycats', createGIF);
-        // console.log(response.data);
-
-        // post request to get an access token
-        // const response = await axios.post('https://api.gfycat.com/v1/oauth/token', token_params);
-        // console.log(`ACCESS TOKEN: ${response.data.access_token}`);
-    }
-
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// async function getToken() {
-//     // let token_params = {
-//     //     client_id: process.env.GYFCAT_ID,
-//     //     client_secret: process.env.GYFCAT_SECRET,
-//     //     grant_type: 'client_credentials'
-//     // };
+module.exports = {
+    name: 'v2g',
+    description: "Sends a video to gfycat api and recieves a gif of it",
+    async execute(client, message, args, Discord) {
+        message.channel.sendTyping();
+        let uploads = [];
+        message.attachments.forEach(attachment => {
+            uploads.push(attachment.url); });
 
-//     // let ACCESS_TOKEN = '';
+        if (uploads.length === 0) { return message.reply("Please upload a video attachment.")}
 
-//     // await axios.post('https://api.gfycat.com/v1/oauth/token', token_params)
-//     //     .then( response => { 
-//     //         // console.log(response.data.access_token)
-//     //         ACCESS_TOKEN = response.data.access_token;
-//     //         });
+        let createGIF = {
+            fetchUrl: `${uploads[0]}`,
+            title: `gif by Discord User ${message.author.id}`
+        };
 
-//     // return ACCESS_TOKEN;
-// }
+        let retgfy = {};
 
-// async function postVideo () {
-//     const response = await 
-// }
+        const response = await axios.post('https://api.gfycat.com/v1/gfycats', createGIF).catch(error => {
+            console.log(error);
+            return message.reply("There was some issue posting given attachment.");
+        });
 
-// exports.getToken = getToken;
+        retgfy ['gfyname'] = response.data.gfyname;
+        message.channel.send(`The gif was uploaded as ${retgfy.gfyname}; encoding in progress (may take some time)...`)
+        
+        let uploadComplete = 0;
+        while (!uploadComplete) {
+            console.log("checking if upload completed...")
+            const response = await axios.get(`https://api.gfycat.com/v1/gfycats/fetch/status/${retgfy.gfyname}`).catch(error => {
+                console.log(error);
+                return message.reply("There was some error checking image status.");
+            });
+
+            if (response.data.task.toLowerCase() === 'complete') { 
+                uploadComplete = 1; 
+                console.log(`${retgfy.gfyname}: upload COMPLETED!`)
+            } else if (response.data.task === "NotFoundo") {
+                return message.reply("There was some error checking image status: NotFoundo.");
+            } else { 
+                console.log(response.data);
+                await sleep(3000);
+                message.channel.sendTyping();
+            }
+        }
+
+        const gifResponse = await axios.get(`https://api.gfycat.com/v1/gfycats/${retgfy.gfyname}`).catch(error => {
+            console.log(error)
+            return message.reply("There was an issue retriving the gif response.")
+        });
+
+        return message.reply(gifResponse.data.gfyItem.gifUrl);
+    }
+}
