3675f92c9c9eb77cb8301b2b2fa374d0aa7d8723
Areeba Khan <kahnareeba@gmail.com>
2022-01-24 23:04:57 -0600

    Talks to mosaic api using axios to send and recieve photo

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const fs = require('fs');
+const fetch = require('node-fetch');
 
 const Discord = require('discord.js');
 const client = new Discord.Client(
---
+++ src/commands/mosaic.js
@@ -0,0 +1,40 @@
+module.exports = {
+    name: 'mosaic',
+    description: "Talks to the API.",
+    async execute(client, message, args, Discord) {
+        const axios = require('axios');
+
+        // test simple get http reqs on axios
+        
+        let uploads = [];
+        message.attachments.forEach(attachment => {
+            uploads.push(attachment.url); });
+
+        if (uploads.length === 0) { return message.reply("Please upload a photo attachment.")}
+
+        let jsonObj = {
+            userID: `${message.author.id}`,
+            imageURL: `${uploads[0]}`
+        };
+
+        let returnObj = {}
+
+        await axios.post('https://flask-tutorial-areebok.herokuapp.com/image', jsonObj)
+            .then(res => {
+                console.log('From /img \t' + res.data);
+                returnObj['img'] = `${res.data}`
+            }).catch(err => console.log(err));
+
+    
+        await axios.get('https://flask-tutorial-areebok.herokuapp.com/pwd')
+            .then(res => console.log('From /pwd \t' + res.data))
+            .catch((err) => console.log(err));
+
+        await axios.get('https://flask-tutorial-areebok.herokuapp.com/ls')
+            .then(res => console.log('From /ls \t' + res.data))
+            .catch((err) => console.log(err));
+
+        return message.reply(`https://flask-tutorial-areebok.herokuapp.com/image/${returnObj.img}`)
+    }
+
+}
