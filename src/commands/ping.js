8ef65889ffc3918acd5a90aa1f3ef740f048314a
Areeba Khan <kahnareeba@gmail.com>
2022-01-29 21:38:52 -0600

    Clears conflict issue and adds thumbs up react

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,15 +1,10 @@
 module.exports = {
     name: "ping",
     description: "this is a ping command",
-<<<<<<< HEAD
-    execute(client, message, args, Discord){
-        message.react('👍');
-        message.channel.send('pong');
-=======
     async execute(client, message, args, Discord){
+        message.react('👍');
         message.channel.sendTyping();
         // sends pong and displays latency. 
         message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
->>>>>>> 53656777ad85a59e5f9cd7b5c954eb2173e82bc2
     }
 }
